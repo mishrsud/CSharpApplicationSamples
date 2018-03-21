603ff474b492c723fdeb8cfb4c867247b428d549
Sudhanshu Mishra <mailsudhanshumishra@gmail.com>
2018-03-22 09:26:08 +1100

    adding xunit support

---
+++ UnitTestWithFeatureFlags/Samples.BusinessLogic.xUnit.Test/JobExecuterShould.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Samples.BusinessLogic.xUnit.Test
+{
+    public class JobExecuterShould
+    {
+        [Fact(Skip = "")]
+        //[Skip()]
+        public void Foo()
+        {
+            
+        }
+    }
+}
